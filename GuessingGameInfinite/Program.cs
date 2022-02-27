1dd3043cbb158a3b4572eaa6884bf0aa410a213f
Margo Parkala <mparkala@tktk.ee>
2022-02-27 23:40:42 +0200

    add new projects

---
+++ GuessingGameInfinite/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace GuessingGameInfinite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //programm genereerib juhuslikku numbrit 1 kuni 10;
+            //kasutaja peab seda numbrit ära arvama;
+            //kui kasutaja suutis seda numbrit ära arvata, siis on ta mängu võitnud;
+            //katsete arv on piiramatu
+            //*programm genereerib juhuslikku numbrit ühe korra;
+
+            Random rnd = new Random();
+            int cpuNumber = rnd.Next(1,11);
+            bool loopActive = true;
+            Console.WriteLine("Sisestage number");
+
+            
+            while(loopActive)
+            {
+ 
+                
+                int userNumber = Convert.ToInt32(Console.ReadLine());
+                
+                if(userNumber==cpuNumber)
+                {
+                    Console.WriteLine("Olete võitnud!");
+                    break;
+                }
+            }
+            
+
+
+
+
+           
+            
+
+   
+            
+
+           
+
+                                                                          
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ GuessingGameThreeTries/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace GuessingGameThreeTries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm genereerib juhuslikku numbrit 1 kuni 10;
+            //kasutaja peab seda numbrit ära arvama;
+            //kui kasutaja suutis seda numbrit ära arvata, siis on ta mängu võitnud;
+            //kasutajal on kolm katset, kui kasutaja ei ole suutnud kolme katsega numbrit ära arvata, siis mängu võidab arvuti;
+            //*programm genereerib juhuslikku numbrit ühe korra;
+
+            Random rnd = new Random();
+            int cpuNumber = rnd.Next(1, 11);
+            Console.WriteLine("Sisestage number");
+
+            int i = 0;
+            
+            while(i<3)
+            {
+                int userNumber = Convert.ToInt32(Console.ReadLine());
+
+                if (userNumber == cpuNumber)
+                {
+                    Console.WriteLine("Olete võitnud");
+                    break;
+                }
+                else
+                {
+                    i++;
+                    Console.WriteLine($"Vale PIN. { 3 - i} katset on jäänud");
+                }
+                                                
+            }
+
+            Console.WriteLine("Kena päeva!");
+
+
+        }
+    }
+}
